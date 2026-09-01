5123adeac2725b8f96f77271b7b7b73c02e54211
Chris/Brayden <cfox11@wvup.edu>
2026-08-31 21:12:41 -0400

    lesson 2 work

--- src/weird_ai/dataset.py
+++ src/weird_ai/dataset.py
@@ -13,5 +13,7 @@ class LyricsDataset:
         # TODO:
         # Get the input/output token sequences
         # Calculate x by grabbing the sublist of tokens starting at the index up to the block_size
+        x = self.tokens[index:index + self.block_size]
         # Calculate y by grabbing the sublist of tokens starting at index + 1 up to block_size + 1
-        pass
+        y = self.tokens[index + 1:index + self.block_size + 1]
+        return torch.tensor(x, dtype=torch.long), torch.tensor(y, dtype=torch.long)
